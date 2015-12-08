14ed8230008051b7c22cf9f51df6b8caa7a6879e
Manuel <wimmer@big.tuwien.ac.at>
2015-12-08 11:11:22 +0100

    Migration from Luna to Mars concerning xText

---
+++ Chapter_7_TCS/mdse.book.swml.tcs/src-gen/mdse/book/sWML/ContentLayer.java
@@ -0,0 +1,43 @@
+/**
+ */
+package mdse.book.sWML;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Content Layer</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * </p>
+ * <ul>
+ *   <li>{@link mdse.book.sWML.ContentLayer#getClasses <em>Classes</em>}</li>
+ * </ul>
+ *
+ * @see mdse.book.sWML.SWMLPackage#getContentLayer()
+ * @model
+ * @generated
+ */
+public interface ContentLayer extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Classes</b></em>' containment reference list.
+   * The list contents are of type {@link mdse.book.sWML.Class}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Classes</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Classes</em>' containment reference list.
+   * @see mdse.book.sWML.SWMLPackage#getContentLayer_Classes()
+   * @model containment="true"
+   * @generated
+   */
+  EList<mdse.book.sWML.Class> getClasses();
+
+} // ContentLayer
